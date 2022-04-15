208e562d498f77364bedb3a0a7f2a60638efa230
Evgeniya Meshuris <em@theemm.com>
2022-04-15 11:10:12 -0700

    add test that should return Human object

--- __test__/test.js
+++ __test__/test.js
@@ -1,6 +1,13 @@
-Describe ('Constructor name', () => {
+describe(Human, () => {
+  test("should return Human object", () => {
+    const human = new Human(50, 75);
+    expect(human.age).toEqual(50);
+  });
+});
 
-  test('should accuratly describe what the test will be doing once passing', () => {
-    
-  })
-})
+test("should return years left on Earth if age is less than life_exp", () => {
+  const human = new Human(50, 75);
+  let age_left = CalcAgeLeft("Earth", human);
+  expect(age_left).toEqual(25);
+});
+
