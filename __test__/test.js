61cd5d3c58a6590d8a913a955c4134858fdf23bd
Evgeniya Meshuris <em@theemm.com>
2022-04-15 11:42:36 -0700

    add test: should return years left on Mars if age is more than life_exp

--- __test__/test.js
+++ __test__/test.js
@@ -22,3 +22,9 @@ test("should return years left on Mars if age is less than life_exp", () => {
   let age_left = CalcAgeLeft("Mars", human);
   expect(age_left).toEqual(47);
 });
+
+test("should return years left on Mars if age is more than life_exp", () => {
+  const human = new Human(80, 75);
+  let age_left = CalcAgeLeft("Mars", human);
+  expect(age_left).toEqual(9.4);
+});
