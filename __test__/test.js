d9b272df085a10111df65f383048c56f4b20b1c9
Evgeniya Meshuris <em@theemm.com>
2022-04-15 11:39:47 -0700

    add test: should return years left on Earth if age is more than life_exp

--- __test__/test.js
+++ __test__/test.js
@@ -11,3 +11,10 @@ test("should return years left on Earth if age is less than life_exp", () => {
   expect(age_left).toEqual(25);
 });
 
+test("should return years left on Earth if age is more than life_exp", () => {
+  const human = new Human(80, 75);
+  let age_left = CalcAgeLeft("Earth", human);
+  expect(age_left).toEqual(5);
+});
+
+
